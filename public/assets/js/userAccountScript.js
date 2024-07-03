7907f0c45610727cefbd272f18f6849df55b939e
gddsnethmal@students.nsbm.ac.lk <gddsnethmal@students.nsbm.ac.lk>
2024-07-03 22:18:49 +0530

    user account and user order tab added

---
+++ public/assets/js/userAccountScript.js
@@ -0,0 +1,23 @@
+document.getElementById('profileImage').addEventListener('click', function() {
+    document.getElementById('imgInput').click();
+});
+
+document.getElementById('imgInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profileImage').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('editbtn').addEventListener('click', function() {
+    var popupForm = document.getElementById('popupForm');
+    var close = document.getElementById('closeButton');
+    popupForm.style.display = 'block';
+    close.onclick = function() {
+        popupForm.style.display = 'none';
+    }
+});
