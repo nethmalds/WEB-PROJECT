337733ccfe0bdf46e776ebc1f2591ca8f63cb1a6
gddsnethmal@students.nsbm.ac.lk <gddsnethmal@students.nsbm.ac.lk>
2024-07-11 11:59:39 +0530

    UserCart page added

--- public/assets/js/src/fetchMedicationData.php
+++ public/assets/js/src/fetchMedicationData.php
@@ -4,7 +4,7 @@ require '../../../../vendor/autoload.php';
 $client = new MongoDB\Client("mongodb://localhost:27017");
 $collection = $client->MEDIX->Medications;
 
-$cursor = $collection->aggregate([ ['$sample' => ['size' => 4]] ]);
+$cursor = $collection->aggregate([ ['$sample' => ['size' => 16]] ]);
 $medications = iterator_to_array($cursor);
 
 foreach ($medications as &$medication) {
